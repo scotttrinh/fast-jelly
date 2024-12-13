3fe0301a02be113db0d9fe0dc3b983b47f2039ee
Scott Trinh <scott@scotttrinh.com>
2024-12-13 14:41:30 -0500

    Oops, add the auth router to the api route

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from fastapi import FastAPI, APIRouter
 
-from app import users, events, ui
+from app import auth, users, events, ui
 
 
 fast_api = FastAPI()
@@ -11,5 +11,6 @@ fast_api.include_router(ui.router)
 api_router = APIRouter()
 api_router.include_router(users.router)
 api_router.include_router(events.router)
+api_router.include_router(auth.router)
 
 fast_api.include_router(api_router, prefix="/api")
